cdf5409d5fbf7c45fc9d693a4c37c1670b6ee7a4
mosheArch <alvaro.moises.diaz.diaz@gmail.com>
2019-12-02 23:06:57 -0600

    Serializer, routes the api Movie and Ratings

--- ratemovie/models.py
+++ ratemovie/models.py
@@ -6,6 +6,9 @@ class Movie (models.Model):
     title = models.CharField(max_length=50)
     description = models.TextField(max_length=400)
 
+    def __str__(self):
+        return self.title
+
 class Rating(models.Model):
     movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
